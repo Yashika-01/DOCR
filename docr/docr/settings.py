4ea6a8fefaa77f8c0513214e734c1ad5219b2557
Nez3 <neha.bharambe3@gmail.com>
2021-09-21 01:30:07 +0530

    postgres setup

--- docr/docr/settings.py
+++ docr/docr/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'docr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'user_info',
+        'USER': 'admin',
+       'PASSWORD': 'admin',
+       'HOST': '127.0.0.1',
+       'PORT': '5432',
+
     }
 }
 
--- docr/ocr/models.py
+++ docr/ocr/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class info(models.Model):
+    name = models.CharField(max_length = 1000)
